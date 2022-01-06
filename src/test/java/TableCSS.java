56cbeaff5520507b0b2634bc9fdcb894ae770b3c
IvanAlekseevv <kropivnisk@gmail.com>
2022-01-06 17:32:00 +0200

    Home work

--- src/test/java/TableCSS.java
+++ src/test/java/TableCSS.java
@@ -18,13 +18,19 @@ public class TableCSS {
     public void tableTest(){
         wd.findElement(By.cssSelector("table"));
         // tr9 - one element
+        wd.findElement(By.cssSelector("table tr:last-child"));
         // Maria Anders - one element
+        wd.findElement(By.cssSelector("table tr:nth-child(2) td:nth-child(2)"));
         // last column - all element
-        // th elements
-        // Centro comercial Moctezuma	FranciscoChang	Mexico - one element
-        // Giovanni Rovelli - one element
-        // first column - one element
 
+        // th elements
+        wd.findElement(By.cssSelector("table tr th"));
+        // Centro comercial Moctezuma	FranciscoChang	Mexico - one element
+        wd.findElement(By.cssSelector("table tr:nth-child(4)"));
+        // Giovanni Rovelli - one element
+        wd.findElement(By.cssSelector("table tr:nth-child(9) td:nth-child(2)"));
+        // first column - all element
+        wd.findElements(By.cssSelector("")); 
 
     }
 }
